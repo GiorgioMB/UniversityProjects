d0e5fa4a95d0a14ba69ba10c2a8060bd6bcaa129
GiorgioMB <122452088+GiorgioMB@users.noreply.github.com>
2023-11-26 09:46:36 +0100

    Update HyperParamOptimization.py

--- BSDSA/ImageCNN/HyperParamOptimization.py
+++ BSDSA/ImageCNN/HyperParamOptimization.py
@@ -1,51 +1,54 @@
 import tensorflow as tf
-import numpy as np
-from sklearn.metrics import confusion_matrix,f1_score, precision_score, recall_score, log_loss
 import os
+import numpy as np
 from sklearn.model_selection import train_test_split
 from kaggle.api.kaggle_api_extended import KaggleApi
 import tensorflow as tf
 from tensorflow.keras.layers import Rescaling, Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras import Sequential
+from tensorflow.keras.utils import image_dataset_from_directory
 from hyperopt import fmin, tpe, hp
+from sklearn.metrics import log_loss
 
 
 
-batch_size = 8 
+# Set parameters
+batch_size = 8 #16 #8
 img_height = 150
 img_width = 150
 num_classes = 6
 num_folds = 5
-###Run only once to download it
+##Run only once
 #os.environ['KAGGLE_CONFIG_DIR'] = '/kaggle.json'
 #dataset_name = 'puneet6060/intel-image-classification'
 #api = KaggleApi()
 #api.authenticate()
 #print("Authenticated, downloading dataset...")
-#api.dataset_download_files(dataset_name, path='/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/', unzip=True) ##Modify this to your preferred location
+#api.dataset_download_files(dataset_name, path='/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/', unzip=True)
 #print("Dataset downloaded")
 ##
 
-
-dataset_test_dir = r"/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/seg_test" ##Use the same you did in "path" before, up until /seg_test"
-dataset_train_dir = r"/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/seg_train" ##Same as before
-train_ds = tf.keras.utils.image_dataset_from_directory(
+dataset_test_dir = r"/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/seg_test/seg_test"
+dataset_train_dir = r"/Users/micalettog/Desktop/Computer Science/Projects/BSDSA/ImageCNN/dataset/seg_train/seg_train"
+train_ds = image_dataset_from_directory(
     dataset_train_dir,
     seed=341,
     image_size=(img_height, img_width),
     batch_size=batch_size
 )
-val_ds = tf.keras.utils.image_dataset_from_directory(
+val_ds = image_dataset_from_directory(
     dataset_test_dir,
     seed=341,
     image_size=(img_height, img_width),
     batch_size=batch_size
 )
-val_true_labels = []  
+val_true_labels = []  # List to store true labels
+
 for images, labels in val_ds:
     val_true_labels.extend(labels.numpy())
 
 def objective(params):
-    model = tf.keras.Sequential([
+    model = Sequential([
         Rescaling(1./255),
         Conv2D(params['num_filters'], params['kernel_size'], activation='relu'),
         MaxPooling2D(pool_size=(2, 2)),
@@ -59,17 +62,19 @@ def objective(params):
         Dense(128, kernel_regularizer=tf.keras.regularizers.l1(params['l1_lambda']), activation='relu'),
         Dense(64, kernel_regularizer=tf.keras.regularizers.l1(params['l1_lambda']), activation='relu'),
         Dense(32, kernel_regularizer=tf.keras.regularizers.l1(params['l1_lambda']), activation='relu'),
-        Dense(6)
+        Dense(6,activation = 'softmax')
     ])
     
+    # Compile the model with the specified learning rate
     optimizer = tf.keras.optimizers.Adam(learning_rate=params['learning_rate'])
-    model.compile(optimizer=optimizer, loss='mean_squared_error', metrics=['mae'])
+    model.compile(optimizer=optimizer, loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics=['accuracy'])
 
-    history = model.fit(
+    # Train the model (you should replace this with your data loading and training logic)
+    model.fit(
         train_ds,
         epochs=16,
         validation_data=val_ds,
-        verbose=1
+        verbose=0
     )
     
     predicted_probabilities = model.predict(val_ds)
@@ -77,7 +82,7 @@ def objective(params):
     val_loss = log_loss(val_true_labels, predicted_probabilities)
     return val_loss
 
-
+# Define the hyperparameter search space
 space = {
     'num_filters': hp.choice('num_filters', [16, 32, 64]),
     'kernel_size': hp.choice('kernel_size', [(3, 3), (5, 5), (7, 7)]),
@@ -93,27 +98,33 @@ best_params = {
     'l1_lambda': best['l1_lambda']
 }
 print(best_params)
-model = tf.keras.Sequential([
-    tf.keras.layers.Rescaling(1./255),
-    tf.keras.layers.Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
-    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    #tf.keras.layers.Conv2D(params['num_filters'], params['kernel_size'], activation='relu'),
-    #tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
-    tf.keras.layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
-    tf.keras.layers.Dense(32, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
-    #tf.keras.layers.Dense(16, activation='relu'),
-    tf.keras.layers.Dense(6)
+
+# num_filters: 16 - 64 step: 16
+# kernel_size: 3, 4, 5
+#learning_rate: e-2, e-4 step: e-2 - e-4 / 300
+##TOFIND best
+model = Sequential([
+    Rescaling(1./255),
+    Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Conv2D(best_params['num_filters'], best_params['kernel_size'], activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Flatten(),
+    Dense(128, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
+    Dense(64, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
+    Dense(32, kernel_regularizer=tf.keras.regularizers.l1(best_params['l1_lambda']), activation='relu'),
+    Dense(16, activation='relu'),
+    Dense(6)
 ])
 model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=best_params['learning_rate']),
         loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         metrics=['accuracy']
     )
+model.summary()
