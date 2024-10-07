590927081645ebaf72a3d5f7930340cbcadd7dc7
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-10-07 20:31:03 +0200

    Create main.py

---
+++ BAINSA/YOLOPaper/main.py
@@ -0,0 +1,35 @@
+import cv2
+from ultralytics import YOLO
+
+model = YOLO('yolov5su.pt')  
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error: Could not open webcam.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to grab frame. Check webcam connection or permissions.")
+        break
+
+    results = model(frame)
+    for r in results:
+        for box in r.boxes:
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            label = model.names[int(box.cls[0])]
+            confidence = box.conf[0].item()
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(frame, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+    cv2.imshow('YOLO Webcam', frame)
+
+    # Press 'q' on the keyboard to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
