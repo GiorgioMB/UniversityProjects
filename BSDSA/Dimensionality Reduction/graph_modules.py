67cff74baae0350d594b5fd7aaecf029034b1db2
Giorgio Micaletto <122452088+GiorgioMB@users.noreply.github.com>
2024-11-13 16:05:15 +0100

    Update and rename graph_module.py to graph_modules.py

--- BSDSA/Dimensionality Reduction/graph_modules.py
+++ BSDSA/Dimensionality Reduction/graph_modules.py
@@ -2,7 +2,6 @@ from torch_geometric.nn import GATConv
 import torch.nn as nn
 from torch.nn import functional as F
 import torch
-## Encoder for the VGAE
 class VGATEncoder(nn.Module):
     def __init__(self, in_channels, hidden_channels, latent_channels, heads=1, dropout=0.2):
         super(VGATEncoder, self).__init__()
@@ -20,21 +19,3 @@ class VGATEncoder(nn.Module):
         mu = self.conv_mu(x, edge_index, edge_weight)
         logvar = self.conv_logvar(x, edge_index, edge_weight)
         return mu, logvar
-    
-    
-## Encoder for the GAE
-class GATEncoder(nn.Module):
-    def __init__(self, in_channels, hidden_channels, latent_channels, heads=1, dropout=0.2):
-        super(GATEncoder, self).__init__()
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.conv1 = GATConv(in_channels, hidden_channels, heads=heads, dropout=dropout).to(self.device)
-        self.conv_2 = GATConv(hidden_channels * heads, latent_channels, heads=1, concat=False, dropout=dropout).to(self.device)
-        self.to(self.device)
-    
-    def forward(self, x, edge_index, edge_weight=None):
-        x, edge_index = x.to(self.device), edge_index.to(self.device)
-        if edge_weight is not None:
-            edge_weight = edge_weight.to(self.device)
-        x = F.elu(self.conv1(x, edge_index, edge_weight))
-        x = self.conv_2(x, edge_index, edge_weight)
-        return x
